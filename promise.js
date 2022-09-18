7f207a6d6bb94678a6a37be89d1349374f8cc85c
Efri Yandani S <efriyandani@gmail.com>
2022-09-18 20:15:31 +0700

    commit file promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+
+  const movies = [...theaterIXX, ...theaterVGC]
+
+  let jumlahEmosi = 0;
+
+  movies.forEach((movie) => {
+    if (movie.hasil === emosi) {
+      jumlahEmosi++;
+    }
+  })
+
+  return jumlahEmosi;
+
+};
 
 module.exports = {
   promiseOutput,
